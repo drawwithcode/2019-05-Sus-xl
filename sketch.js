2dc0439f449c49080ad545a08f3cb7f3eb6adbd8
Sus-xl <55488310+Sus-xl@users.noreply.github.com>
2019-10-22 20:03:23 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,37 @@
-function preload(){
-  // put preload code here
-}
+var capture;
+let ellipseColor;
+var myImage;
 
 function setup() {
-  // put setup code here
+	createCanvas(windowWidth, windowHeight);
+	capture = createCapture(VIDEO);
+	capture.size(windowWidth, windowHeight);
+	capture.hide();
+  background(244, 248, 252);
+
+  button = createButton('Click here to take a picture, then press mouse to draw.');
+    button.position(windowWidth/3, 19);
+    button.mousePressed(changeBG);
 }
 
 function draw() {
-  // put drawing code here
+  stroke('#3d3d3d');
+  strokeWeight(10);
+  if (mouseIsPressed === true) {
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+
+
 }
+
+function changeBG() {
+  clear();
+  var myImage = capture.loadPixels();
+	image(myImage, width/4, height/6, 640, 480);
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+s
